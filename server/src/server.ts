a27eda4c7e07837ca44e501590cae9cd9dc68f02
DeyvidDiniz <deyvidatd@hotmail.com>
2023-05-17 13:35:49 -0300

    Configuração Backend

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('😮 HTTP server runnig on http://localhost:3333')
+  })
