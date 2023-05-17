241ddce571feb5e997852517805b509658c81742
DeyvidDiniz <deyvidatd@hotmail.com>
2023-05-17 17:49:19 -0300

    Configuração Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-gray-700 flex-1">
+      <Text>Teste 2</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
